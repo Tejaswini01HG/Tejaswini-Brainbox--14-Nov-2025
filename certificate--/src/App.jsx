069a57f9cd7b63cdc4cb90f749e98830d2c79ff8
Tejaswini01HG <hgtejaswini913@gmail.com>
2025-11-14 19:12:49 +0530

    Add files via upload

---
+++ certificate--/src/App.jsx
@@ -0,0 +1,11 @@
+// App.jsx
+import React from "react";
+import CertificateGenerator from "./CertificateGenerator.jsx";
+
+
+function App() {
+  return <CertificateGenerator />;
+}
+
+export default App;
+
